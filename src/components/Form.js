335bccda2eb283418bf6d80204e2374a4e80e71e
tkl <tluk92@gmail.com>
2022-08-20 03:11:40 -0400

    wip: basic construction

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import {useState} from "react"
+
+export default function Form (){
+    const [state, setState] = useState(null);
+
+    return(
+        <>
+        <form>
+            <input type="text" name="" value="" onChange="" />
+        </form>
+        </>
+    )
+}
